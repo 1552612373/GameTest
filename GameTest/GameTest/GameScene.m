d338906e4b2168609c5b31ee94b13b272ca842c2
yang <1552612373@qq.com>
2017-03-20 18:02:10 +0800

    a

--- GameTest/GameTest/GameScene.m
+++ GameTest/GameTest/GameScene.m
@@ -8,6 +8,14 @@
 
 #import "GameScene.h"
 
+#define ARC4RANDOM_MAX 0x100000000
+
+// 随机数
+static inline CGFloat ScalarRandomRange(CGFloat min, CGFloat max)
+{
+    return floorf(((double)arc4random() / ARC4RANDOM_MAX) * (max - min) + min);
+}
+
 static const float ZOMBIE_MOVE_POINTS_PER_SEC = 240.0;
 
 static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b)
@@ -61,7 +69,7 @@ static inline CGFloat ScalarShortestAngleBetween(const CGFloat a, const CGFloat 
     return angle;
 }
 
-static const float ZOMBIE_ROTATE_RADIANS_PER_SEC = 4 * M_PI;
+static const float ZOMBIE_ROTATE_RADIANS_PKER_SEC = 4 * M_PI;
 
 @implementation GameScene
 {
@@ -82,7 +90,7 @@ static const float ZOMBIE_ROTATE_RADIANS_PER_SEC = 4 * M_PI;
 //        bgSprite.position = CGPointZero;
         bgSprite.zRotation = M_PI/8;
         [self addChild:bgSprite];
-        [self createOneNewSprite];
+        [self runAction:[SKAction repeatActionForever: [SKAction sequence:@[[SKAction performSelector:@selector(createOneNewSprite) onTarget:self],[SKAction waitForDuration:2.0]]]]];
         
         NSLog(@"%f %f",self.size.width,self.size.height);
         
@@ -225,51 +233,82 @@ static const float ZOMBIE_ROTATE_RADIANS_PER_SEC = 4 * M_PI;
 }
 
 
+
 - (void)createOneNewSprite
 {
     SKSpriteNode *enemy = [SKSpriteNode spriteNodeWithImageNamed:@"monster"];
-    enemy.position = CGPointMake(self.size.width + enemy.size.width/2, self.size.height/2);
+    enemy.position = CGPointMake(self.size.width + enemy.size.width/2,
+                                 ScalarRandomRange(enemy.size.height/2,
+                                                   self.size.height-enemy.size.height/2));
+
     [self addChild:enemy];
     
-//    // 1
-//    SKAction *actionMidMove =
-//    [SKAction moveTo:CGPointMake(self.size.width/2,
-//                                 enemy.size.height/2)
-//            duration:1.0];
-//    // 2
-//    SKAction *actionMove =
-//    [SKAction moveTo:CGPointMake(-enemy.size.width/2,
-//                                 enemy.position.y) duration:1.0];
-    
-    // 这个是可逆的action  上面两个moveTo是不可逆的
-    SKAction *actionMidMove = [SKAction moveByX:-self.size.width/2-enemy.size.width/2
-                    y:-self.size.height/2+enemy.size.height/2
-                    duration:1.0];
-    
-    SKAction *actionMove = [SKAction moveByX:-self.size.width/2-enemy.size.width/2
-                    y:self.size.height/2+enemy.size.height/2
-                    duration:1.0];
-    
-    SKAction *wait = [SKAction waitForDuration:0.25];
-    
-    SKAction *logMessage = [SKAction runBlock:^{
-        
-        NSLog(@"Reached bottom!");
-    }];
     
     
-    SKAction *reverseMid = [actionMidMove reversedAction];
     
-    SKAction *reverseMove = [actionMove reversedAction];
     
-    // 3
-//    SKAction *sequence = [SKAction sequence:@[actionMidMove, logMessage, wait, actionMove, reverseMove, logMessage, wait ,reverseMid]];
-    // 或者
-    SKAction *sequence = [SKAction sequence:@[actionMidMove, logMessage, wait, actionMove]];
-    sequence = [SKAction sequence:@[sequence,[sequence reversedAction]]];
+    SKAction *actionMove = [SKAction moveToX:-enemy.size.width/2 duration:2.0];
+    SKAction *actionRemove = [SKAction removeFromParent];
+    [enemy runAction:[SKAction sequence:@[actionMove, actionRemove]]];
     
-    // 4
-    [enemy runAction:sequence];
+    
+    
+    
+    
 }
+                                 
+//// 基础action例子
+//- (void)createOneNewSprite
+//{
+//    SKSpriteNode *enemy = [SKSpriteNode spriteNodeWithImageNamed:@"monster"];
+//    enemy.position = CGPointMake(self.size.width + enemy.size.width/2, self.size.height/2);
+//    [self addChild:enemy];
+//    
+////    // 1
+////    SKAction *actionMidMove =
+////    [SKAction moveTo:CGPointMake(self.size.width/2,
+////                                 enemy.size.height/2)
+////            duration:1.0];
+////    // 2
+////    SKAction *actionMove =
+////    [SKAction moveTo:CGPointMake(-enemy.size.width/2,
+////                                 enemy.position.y) duration:1.0];
+//    
+//    // 这个是可逆的action  上面两个moveTo是不可逆的
+//    SKAction *actionMidMove = [SKAction moveByX:-self.size.width/2-enemy.size.width/2
+//                    y:-self.size.height/2+enemy.size.height/2
+//                    duration:1.0];
+//    
+//    SKAction *actionMove = [SKAction moveByX:-self.size.width/2-enemy.size.width/2
+//                    y:self.size.height/2+enemy.size.height/2
+//                    duration:1.0];
+//    
+//    SKAction *wait = [SKAction waitForDuration:0.25];
+//    
+//    SKAction *logMessage = [SKAction runBlock:^{
+//        
+//        NSLog(@"Reached bottom!");
+//    }];
+//    
+//    
+//    SKAction *reverseMid = [actionMidMove reversedAction];
+//    
+//    SKAction *reverseMove = [actionMove reversedAction];
+//    
+//    // 3
+//    // 逆action
+////    SKAction *sequence = [SKAction sequence:@[actionMidMove, logMessage, wait, actionMove, reverseMove, logMessage, wait ,reverseMid]];
+//    // 或者
+//    SKAction *sequence = [SKAction sequence:@[actionMidMove, logMessage, wait, actionMove]];
+//    sequence = [SKAction sequence:@[sequence,[sequence reversedAction]]];
+//    
+//    
+//    // 重复某个动作
+//    SKAction *repeat = [SKAction repeatActionForever:sequence];
+//    [enemy runAction:repeat];
+//    
+//    // 4
+//    [enemy runAction:sequence];
+//}
 
 @end
